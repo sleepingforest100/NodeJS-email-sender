c3206ed62a56dad372be31e053bf395c0c75d748
Madina <123440519+sleepingforest100@users.noreply.github.com>
2024-02-21 14:28:13 +0600

    Feature: environmental variables, complex email content

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
 "use strict";
 const nodemailer = require("nodemailer");
 const fs = require("fs");
- require('dotenv').config();
-
-
+require('dotenv').config();
 
 const emailUser = process.env.EMAIL_USER;
 const emailPass = process.env.EMAIL_PASS;
 
-async function notifyAdmin(){
+async function notifyAdmin() {
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: "mmedikoshh@gmail.com",
+          pass: "rbnmglhrqmfccmfa",
+        },
+    });
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    
-    user: userEmail,
-    pass: pass,
-  },
-});
+    const htmlTemplate = `
+        <h1>Received a new message</h1>
+        <p>Hello Admin,</p>
+        <p>A new message has been received.</p>
+    `;
 
+    const info = await transporter.sendMail({
+        from: "mmedikoshh@gmail.com",
+        to: "220576@astanait.edu.kz",
+        subject: "Successful sending of a new message",
+        html: htmlTemplate,
+        attachments: [
+            {
+                filename: 'file.txt',
+                content: fs.createReadStream('D:/Programs/VSCode Projects/email_sender/file.txt'), // Path to your file
+            }
+        ]
+    });
 
-
-  const info = await transporter.sendMail({
-    from: userEmail, 
-    to: userEmail, 
-    subject: "Successful sending of message", 
-   
-    html: `
-    <h1>Received message</h1>
-    `,
-
-    attachments: [
-      {
-          filename: 'file.txt',
-          content: fs.createReadStream('paths'), // Path to your file
-      }
-  ]
-  });
-
-  console.log("Message sent: %s", info.messageId);
+    console.log("Message sent: %s", info.messageId);
 }
 
 async function main() {
-    await notifyAdmin()
+    try {
+        await notifyAdmin();
+    } catch (error) {
+        console.error("Error sending email:", error);
+    }
 }
 
-main().catch(console.error);
+main();
